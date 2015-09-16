8cdac8cd928a40774eb7831476c7adc03617deae
Mawaheb Seraj <mawaheb.seraj@devtechgroup.com>
2015-09-16 14:02:51 +0200

    Add landingPage view.

--- App/view/landingPage.js
+++ App/view/landingPage.js
@@ -1,3 +1,40 @@
 /**
  * Created by mawaheb.seraj on 9/16/2015.
  */
+Ext.onReady(function(){
+	var viewport = new Ext.Viewport({
+		layout: 'border',
+		renderTo: Ext.getBody(),
+		items: [{
+			region: 'north',
+			xtype: 'panel',
+			html: 'North (Menus go here.)'
+		},{
+			region: 'west',
+			xtype: 'panel',
+			split: 'true',
+			width: 200,
+			html: 'West (Lists go here)'
+		},{
+			region: 'center',
+			xtype: 'panel',
+			items:[{
+				xtype: 'button',
+				text: 'Users',
+				cls: 'btn-big'
+			},{
+				xtype: 'button',
+				text: 'Jobs',
+				cls: 'btn-big'
+			},{
+				xtype: 'button',
+				text: 'Settings',
+				cls: 'btn-big'
+			},{
+				xtype: 'button',
+				text: 'Lucky Guy!',
+				cls: 'btn-big'
+			}]
+		}]
+	});
+});
